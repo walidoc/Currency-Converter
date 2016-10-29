4917d3336d2869e241176282c4c954d326df92c7
Walid Rahali <walidrahali10@gmail.com>
2016-10-29 15:30:22 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ angular.module('myApp', [])
   .controller('ConversionController', ['$scope','$http', function($scope, $http){ 
 
       $scope.rates= {};
-      $http.jsonp("https://api.fixer.io/latest?base=ZAR")
-      .then(function(response){ 
+      $http.get("https://api.fixer.io/latest?base=ZAR")
+      .success(function(response){ 
           $scope.rates = response.data.rates;
           $scope.toType= $scope.rates.AUD;
           $scope.fromType = $scope.rates.USD;
